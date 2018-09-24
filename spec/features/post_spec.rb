2481d7aa5a2fc6ee2512d7480a26467f134ef0f9
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-09-24 20:14:15 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,7 +6,7 @@ describe 'navigate' do
   end
 
   it 'shows the title on the show page in a h1 tag' do
-    visit post_path(@post)
+    visit posts_path(@post)
     expect(page).to have_css("h1", text: "My Post")
   end
 
